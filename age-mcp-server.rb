4e447bcab5857e515c72802730f48f20356fc034
Rio Fujita <rifujita@microsoft.com>
2025-10-31 09:48:25 +0900

    Dependency Update

--- age-mcp-server.rb
+++ age-mcp-server.rb
@@ -3,15 +3,15 @@ class AgeMcpServer < Formula
 
   desc "Apache AGE MCP Server"
   homepage "https://github.com/rioriost/homebrew-age-mcp-server/"
-  url "https://files.pythonhosted.org/packages/df/e8/a32013c491a4df9a1fe3b57c0fd237a4f4e14467684ac17aaf6f65f2346c/age_mcp_server-0.2.29.tar.gz"
-  sha256 "9abd2c01c4a673750ffdd580d7d4e3ae7aab208efb44628ae9e81198963441d9"
+  url "https://files.pythonhosted.org/packages/cf/c4/2809265de03d52d86780d39689548d8a8bb7868c6d1b6a798dc082fd76b1/age_mcp_server-0.2.30.tar.gz"
+  sha256 "0646a09e4712180d3d858a9a9562895a140d12f6739db2efe9366df1ad9c32af"
   license "MIT"
 
   depends_on "python@3.13"
 
   resource "agefreighter" do
-    url "https://files.pythonhosted.org/packages/c6/e5/5038fb1470c2525f5879c9e61d012915a3f46e17ae402ce7eb310ce941ee/agefreighter-1.0.18.tar.gz"
-    sha256 "bcbfe8a6c1318847e2075a22287f88851951288326ca9fb75f3009872ccb4b5a"
+    url "https://files.pythonhosted.org/packages/15/11/04078a86da72ac1106b61fde6aa88dd4843732b043652a20bb615859b15c/agefreighter-1.0.19.tar.gz"
+    sha256 "ec2cd4f2b415d5378c80ef7fe7f64bedd50641fa5f0f7257a3eddf79711b5adc"
   end
 
   resource "ply" do
